9b92ed8bc86ddaf97fe006da9181cac2c834b1d6
@hexlet/cli <support@hexlet.io>
2022-12-11 16:50:37 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -1,21 +1,17 @@
 package exercise;
 
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // BEGIN
 class App {
-//    public static void main(String[] args) {
-//        scrabble("rkqodlw", "world");
-//    }
-
     public static boolean scrabble(String str, String word) {
-        List<String> list1 = Arrays.asList(str.split(""));
+        List<String> list1 = new ArrayList<>(Arrays.asList(str.split("")));
         String[] list2 = word.split("");
 
         for (String s: list2) {
-            if (list1.contains(s)) {
+            if (list1.contains(s.toLowerCase())) {
                 list1.remove(s);
             } else {
                 return false;
